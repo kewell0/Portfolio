4902787af79bd5359c9cff8f86bcf9a9e5d255a4
CALEB <owatahcaleb@gmail.com>
2023-08-31 17:39:29 +0100

    scroll update

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -133,7 +133,7 @@ export default function Navbar() {
         <div
           className={
             nav
-              ? "fixed left-0 top-0 w-[75%] sm:w-[60%] md:[45%] h-screen bg-[#ecf0f3] p-10 ease-in duration-500"
+              ? "fixed left-0 top-0 w-[75%] sm:w-[60%] md:[45%] h-[100%] bg-[#ecf0f3] p-10 ease-in duration-500"
               : "fixed left-[-100%] top-0 p-10 ease-in duration-500"
           }
         >
