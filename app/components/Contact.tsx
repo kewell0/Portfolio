adecf20a17b811e58b4765926fff61c743715dc6
CALEB <owatahcaleb@gmail.com>
2023-11-09 22:20:17 +0100

    added email.js for contact form

--- app/components/Contact.tsx
+++ app/components/Contact.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import { Link as LinkScroll } from "react-scroll";
 import Image from "next/image";
 import Link from "next/link";
@@ -9,8 +9,31 @@ import { BsFillPersonLinesFill } from "react-icons/bs";
 import { FaGithub, FaLinkedinIn } from "react-icons/fa";
 import { HiOutlineChevronDoubleUp } from "react-icons/hi";
 import ContactImg from "../../public/assets/contact.jpg";
+import emailjs from "@emailjs/browser";
 
 const Contact = () => {
+  const form = useRef("");
+
+  const sendEmail = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_0552xld",
+        "template_ciobmdd",
+        form.current,
+        "PD92TtfFHhRLAIyZl"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
+
   return (
     <div id="contact" className="w-full lg:h-screen lg:px-16">
       <div className="max-w-[1240px] m-auto px-2 py-16 w-full ">
@@ -73,11 +96,7 @@ const Contact = () => {
           {/* right */}
           <div className="col-span-3 w-full h-auto shadow-xl shadow-gray-400 rounded-xl lg:p-4">
             <div className="p-4">
-              <form
-                action="https://getform.io/f/08ebcd37-f5b5-45be-8c13-714f011ce060"
-                method="POST"
-                encType="multipart/form-data"
-              >
+              <form ref={form} onSubmit={sendEmail}>
                 <div className="grid md:grid-cols-2 gap-4 w-full py-2">
                   <div className="flex flex-col">
                     <label className="uppercase text-sm py-2">Name</label>
