81e55615b6d98640d2c9851b650e379784c930d0
Caleb Owatah <owatahcaleb@gmail.com>
2024-04-08 15:18:52 +0100

    skills edit

--- app/components/Skills.tsx
+++ app/components/Skills.tsx
@@ -1,3 +1,4 @@
+"use client";
 import React from "react";
 import Image from "next/image";
 import Html from "../../public/assets/skills/html.png";
@@ -11,6 +12,20 @@ import NextJS from "../../public/assets/skills/nextjs.png";
 import AWS from "../../public/assets/skills/aws.png";
 
 const Skills = () => {
+  const [isSmallScreen, setIsSmallScreen] = React.useState(false);
+
+  React.useEffect(() => {
+    const handleResize = () => {
+      setIsSmallScreen(window.innerWidth <= 409);
+    };
+
+    handleResize();
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
   return (
     <div id="skills" className="w-full lg:h-screen p-2">
       <div className="max-w-[1240px] mx-auto flex flex-col justify-center h-full lg:px-16">
@@ -43,10 +58,10 @@ const Skills = () => {
           <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
             <div className="grid grid-cols-2 gap-4 justify-center items-center">
               <div className="m-auto">
-                <Image src={Javascript} width="64" height="64" alt="/" />
+                <Image src={Javascript} width="58" height="58" alt="/" />
               </div>
               <div className="flex flex-col items-center justify-center">
-                <h3>JavaScript</h3>
+                <h3>{isSmallScreen ? "JS" : "JavaScript"}</h3>
               </div>
             </div>
           </div>
